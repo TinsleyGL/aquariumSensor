66f230a67c7bc6ad37155175971fb372876e7a59
Gareth Tinsley <tinsleyuni@gmail.com>
2019-08-17 14:57:01 +0100

    requests added

--- dataLogger.py
+++ dataLogger.py
@@ -1,19 +1,32 @@
 import threading
 import random
+import requests
 
+userID = '1'
+aquariumName = 'Downstairs'
 
 def temperatureRead():
-    print (random.randint(20,26))
+    return (random.randint(20,26))
 
 def phRead():
-    print (random.randint(6,8))
+    return (random.randint(6,8))
 
 def getValues(): 
     threading.Timer(10, getValues).start()
-    temperatureRead()
-    phRead()
+    temp = temperatureRead()
+    ph = phRead()
 
+    try:
+        r = requests.post('http://127.0.0.1:5000/data', data = {
+            'UserID': userID,
+            'Aquarium': aquariumName,
+            'Temperature': temp,
+            'PH': ph
+        })
+        r.raise_for_status()
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print (err)
 
-
-
+    #print(threading.activeCount())
+    #print(threading.enumerate())
 
